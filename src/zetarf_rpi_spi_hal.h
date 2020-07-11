4d19d4438dd0f5336cd112189f622dd87ce658e7
Christopher <hallgchris@gmail.com>
2020-07-11 11:42:48 +1000

    Fix indentation in RPi HAL

--- src/zetarf_rpi_spi_hal.h
+++ src/zetarf_rpi_spi_hal.h
@@ -21,11 +21,11 @@ namespace ZetaRFHal {
 template <class TChipSelectPin, class TShutdownPin, class TIrqPin>
 class RPiSpiHal
 {
-	static_assert(std::is_base_of<ChipSelectPinSelector, TChipSelectPin>::value, "First parameter of ZetaRF must be the Chip Select pin. Use ChipSelectPin<10> to use pin 10.");
-	static_assert(std::is_base_of<ShutdownPinSelector, TShutdownPin>::value, "Second parameter of ZetaRF must be the Shutdown pin. Use ShutdownPin<9> to use pin 9.");
-	static_assert(std::is_base_of<IrqPinSelector, TIrqPin>::value, "Third parameter of ZetaRF must be the IRQ pin. Use IrqPin<8> to use pin 8.");
+    static_assert(std::is_base_of<ChipSelectPinSelector, TChipSelectPin>::value, "First parameter of ZetaRF must be the Chip Select pin. Use ChipSelectPin<10> to use pin 10.");
+    static_assert(std::is_base_of<ShutdownPinSelector, TShutdownPin>::value, "Second parameter of ZetaRF must be the Shutdown pin. Use ShutdownPin<9> to use pin 9.");
+    static_assert(std::is_base_of<IrqPinSelector, TIrqPin>::value, "Third parameter of ZetaRF must be the IRQ pin. Use IrqPin<8> to use pin 8.");
  
-	static constexpr int ChipSelect_pin = TChipSelectPin::pin;
+    static constexpr int ChipSelect_pin = TChipSelectPin::pin;
     static constexpr int ShutdownPin = TShutdownPin::pin;
     static constexpr int IrqPin = TIrqPin::pin;
 
@@ -48,92 +48,92 @@ public:
     }
     void deinitialize()
     {
-		pinMode(ChipSelect_pin, INPUT);
+        pinMode(ChipSelect_pin, INPUT);
         pinMode(IrqPin, INPUT);
         pinMode(ShutdownPin, INPUT);
     }
 
-	bool isIrqAsserted() const {
+    bool isIrqAsserted() const {
         return (digitalRead(IrqPin) == LOW);
     }
-	void waitIrq();
-	bool waitIrqFor(unsigned long timeout_ms);
+    void waitIrq();
+    bool waitIrqFor(unsigned long timeout_ms);
 
-	void putInShutdown() {
+    void putInShutdown() {
         digitalWrite(ShutdownPin, HIGH);
     }
-	void releaseFromShutdown() {
+    void releaseFromShutdown() {
         digitalWrite(ShutdownPin, LOW);
     }
 
-	bool waitForClearToSend(uint8_t readCommandId, uint16_t timeout_ms)
-	{
-		unsigned long const t { millis() };
-		uint8_t ctsVal {0};
-		do {
-			restartOrBeginSpiTransaction();
-			spiWriteByte(readCommandId);
-			ctsVal = spiReadByte();
+    bool waitForClearToSend(uint8_t readCommandId, uint16_t timeout_ms)
+    {
+        unsigned long const t { millis() };
+        uint8_t ctsVal {0};
+        do {
+            restartOrBeginSpiTransaction();
+            spiWriteByte(readCommandId);
+            ctsVal = spiReadByte();
 
-			if ((millis()-t) > timeout_ms) {
-				endSpiTransaction();
-				return false;
-			}
-		} while (ctsVal != 0xFF); // Clear to send when 0xFF
+            if ((millis()-t) > timeout_ms) {
+                endSpiTransaction();
+                return false;
+            }
+        } while (ctsVal != 0xFF); // Clear to send when 0xFF
 
-		// Hold SPI transaction
-		return true;
-	}
+        // Hold SPI transaction
+        return true;
+    }
 
-	void beginSpiTransaction()
-	{
+    void beginSpiTransaction()
+    {
         m_inSpiTransaction = true;
         digitalWrite(ChipSelect_pin, LOW);
         //delayMicroseconds(1);
     }
-	void resumeOrBeginSpiTransaction()
-	{
+    void resumeOrBeginSpiTransaction()
+    {
         if (m_inSpiTransaction)
             return;
         beginSpiTransaction();
     }
-	void restartOrBeginSpiTransaction()
-	{
+    void restartOrBeginSpiTransaction()
+    {
         if (m_inSpiTransaction) {
             endSpiTransaction();
             delayMicroseconds(100);
         }
         beginSpiTransaction();
     }
-	void endSpiTransaction()
-	{
+    void endSpiTransaction()
+    {
         delayMicroseconds(1);
         digitalWrite(ChipSelect_pin, HIGH);
         m_inSpiTransaction = false;
     }
 
-	uint8_t spiReadWriteByte(uint8_t value) {
+    uint8_t spiReadWriteByte(uint8_t value) {
         wiringPiSPIDataRW(ZETARF_SPI_SETTINGS_CHANNEL, &value, 1);
         return value;
     }
 
-	void spiWriteByte(uint8_t value) {
+    void spiWriteByte(uint8_t value) {
         spiReadWriteByte(value);
     }
-	uint8_t spiReadByte() {
+    uint8_t spiReadByte() {
         return spiReadWriteByte(0xFF);
     }
 
-	void spiReadWriteData(uint8_t* data, uint8_t count) {
+    void spiReadWriteData(uint8_t* data, uint8_t count) {
         wiringPiSPIDataRW(ZETARF_SPI_SETTINGS_CHANNEL, data, count);
     }
-	void spiWriteData(uint8_t const* data, uint8_t count) {
+    void spiWriteData(uint8_t const* data, uint8_t count) {
         // Make copy of data as wiringPi will overwrite
         uint8_t dataCopy[count];
         memcpy(dataCopy, data, sizeof(dataCopy));
         wiringPiSPIDataRW(ZETARF_SPI_SETTINGS_CHANNEL, dataCopy, count);
     }
-	void spiReadData(uint8_t* data, uint8_t count) {
+    void spiReadData(uint8_t* data, uint8_t count) {
         std::fill_n(data, count, 0xFF);
         wiringPiSPIDataRW(ZETARF_SPI_SETTINGS_CHANNEL, data, count);
     }
@@ -145,7 +145,7 @@ public:
 template <class TChipSelectPin, class TShutdownPin, class TIrqPin, class TClearToSendPin>
 class RPiSpiHal_Gpio1AsClearToSend : RPiSpiHal<TChipSelectPin, TShutdownPin, TIrqPin>
 {
-	static_assert(std::is_base_of<ClearToSendPinSelector, TClearToSendPin>::value, "Fourth parameter of ZetaRF must be the Clear To Send pin. Use ClearToSendPin<11> to use pin 11.");
+    static_assert(std::is_base_of<ClearToSendPinSelector, TClearToSendPin>::value, "Fourth parameter of ZetaRF must be the Clear To Send pin. Use ClearToSendPin<11> to use pin 11.");
 
     static constexpr int ClearToSend_pin = TClearToSendPin::pin;
 
@@ -162,17 +162,17 @@ public:
     }
 
     bool waitForClearToSend(uint8_t readCommandId, uint16_t timeout_ms)
-	{
-		(void)readCommandId;
-		unsigned long const t { millis() };
-		while (digitalRead(ClearToSend_pin) == LOW) {
-			// Wait with timeout...
-			if ((millis()-t) > timeout_ms) {
-				return false;
-			}
-			delayMicroseconds(100);
-		}
-		return true;
+    {
+        (void)readCommandId;
+        unsigned long const t { millis() };
+        while (digitalRead(ClearToSend_pin) == LOW) {
+            // Wait with timeout...
+            if ((millis()-t) > timeout_ms) {
+                return false;
+            }
+            delayMicroseconds(100);
+        }
+        return true;
     }
 };
 
